335dafc5de449876d68faf3989b42caba1440538
alba-sb <albadevilar@gmail.com>
2026-08-20 12:12:52 +0200

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -1,6 +1,6 @@
 // ---------- Navbar titles
 const enNavbarData = {
-  title: " Your name ",
+  title: " Alba Sendón Blanco ",
   Home: "Home",
   publications: "publications",
   Research: "Research",
@@ -10,25 +10,22 @@ const enNavbarData = {
 
 // ---------- Home page data
 const enHomePageData = {
-  name: " Your name ",
-  jobTitle: " Job title ",
+  name: " Alba Sendón Blanco ",
+  jobTitle: " PhD student, Vrije Universiteit Amsterdam ",
   home_title: "About me",
   home_content: `
   <div>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-    <h2 class='title'> Main works </h2>
+    <p> Hello! I am Alba, a PhD student at the Vrije Universiteit, in Amsterdam, working under the supervision of Renee Hoekzema and Thomas Rot. </p>
+    <p> My research interests lie in the algebraic topology of manifolds. Namely, I like studying cobordisms between nested manifolds (manifolds with manifolds inside), as well as cutting and pasting equivariant manifolds.</p>
+    <p> Before my PhD, I studied a Master in Advanced Mathematics at Universitat de Barcelona and a Grao en Matemáticas at Universidade de Santiago de Compostela.</p>
+    <p>On the academic year 2026/2027, I am on the job market. My CV is available upon request.</p>
+    <h2 class='title'> News? </h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh  </p>
-    <ul>
-      <li> Heuristics</li>
-      <li> Policy planning </li>
-      <li> Flexiblity to details </li>
-    </ul>
   </div>
   `,
 };
+
+
 
 // ---------- Publications page data
 const enPublicationsPageData = {

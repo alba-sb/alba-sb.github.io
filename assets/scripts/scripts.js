02ff35897c4638ca513f40509a09b7b75311f0b7
alba-sb <albadevilar@gmail.com>
2026-08-31 17:03:10 +0200

    Update scripts.js

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -218,44 +218,12 @@ $(document).ready(function () {
   // Jobs page data
   function jobsData() {
     document.getElementById("page_title").innerText =
-      lang === "en" ? "Jobs" : " سوابق شغلی ";
+      lang === "en" ? "Conferences" : "Conferencias";
 
-    document.getElementById("jobs_title").innerHTML =
+    document.getElementById("research_title").innerHTML =
       lang === "en" ? enJobsPageData.title : faJobsPageData.title;
-
-    document.getElementById("jobs_data").innerHTML = (
-      lang === "en" ? enJobsPageData.items : faJobsPageData.items
-    )
-      .map(
-        (job) =>
-          `<div class='job_item'>
-            <div class='job_header'>
-              <div>
-                <h1>${job.title}${lang === "en" ? "," : "،"}</h1>
-                <h2> ${job.company}</h2>
-              </div>
-              <div>
-                <span>${job.startData} - ${
-            job.endDate ? job.endDate : lang === "en" ? "Now" : " تاکنون "
-          }</span>
-                <span class='job_location'>${job.location}</span>
-              </div>
-            </div>
-            <p>${job.abstract}</p>
-            ${
-              job.achievements.length > 0
-                ? `<div class="job_achievements">
-                  <ul>
-                    ${job.achievements
-                      .map((achievement) => `<li>${achievement}</li>`)
-                      .join("")}
-                  </ul>
-                </div>`
-                : ""
-            }
-        </div>`
-      )
-      .join("");
+    document.getElementById("research_data").innerHTML =
+      lang === "en" ? enJobsPageData.content : faPageDataJobs.content;
   }
 
   if (pathname === "/jobs") {
